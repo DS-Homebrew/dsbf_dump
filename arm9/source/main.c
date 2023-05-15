2551fa193aa24a74b3d5774a8b9eec0f861cb619
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-05-15 00:15:38 -0700

    v1.2.0

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -274,7 +274,7 @@ int main(int argc, char **argv)
 	consoleInit(&bottomScreen, 3,BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
 
 	consoleSelect(&topScreen);
-	printf(" NDS B+F dumper 1.1.1\n");
+	printf(" NDS B+F dumper 1.2.0\n");
 	printf("=------------------=\n");
 
 	if(!fatInitDefault()) {
